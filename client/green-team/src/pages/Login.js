14742b750be844e924fd99fcc89b06b39d32deaf
egoraver <Averyaskin2001@gmail.com>
2024-03-17 19:27:39 +0700

    Добавление ссылок на кнопки

--- client/green-team/src/pages/Login.js
+++ client/green-team/src/pages/Login.js
@@ -1,12 +1,14 @@
+import { NavLink } from "react-router-dom";
+
 function Login () {
     return (
         <div className="login-main main-block"> 
                 <div className="info-login info-block">            
-                    <p><input className="input-login input-text" placeholder="Эл. почта:" type="text"></input></p>
+                    <p><input className="input-login input-text" placeholder="Эл. почта:" type="email"></input></p>
                     <p><input className="input-login input-text" placeholder="Пароль:" type="password"></input></p>
                     <span><button className="btn-primary btn-google">Войти через Google</button></span>
                     <span><button className="btn-primary">Войти</button></span>
-                    <p><label className="btn-second">Зарегистрироваться</label></p>
+                  <NavLink to="/registration" className="btn-second"><p><label >Зарегистрироваться</label></p></NavLink> 
                 </div>
             </div>
 
